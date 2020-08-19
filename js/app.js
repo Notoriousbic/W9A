fee51dc817b83ea0741f75bbdbfad53baf70bba7
MacPro <macpro@MacPros-MacBook-Pro.local>
2020-08-18 21:16:41 -0600

    W9A update

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 var age = 19;
 var is_subscribed = true;
 
-if (age < 18 && is_subscribed == true) {
+if (age <= 18 && is_subscribed == true) {
     console.log('The User is under 18 and is subscribed');
-} else if (age > 18 && is_subscribed == true) { 
+} else if (age >= 18 && is_subscribed == true) { 
     console.log('The User is an adult and is subscribed');
-} else if (age < 18 && is_subscribed == false){
+} else if (age <= 18 && is_subscribed == false){
     console.log('the User is under 18 and is not subscribed');
-} else if (age > 18 && is_subscribed == false) {
+} else if (age >= 17 && is_subscribed == false) {
     console.log('the user is an adult is not subscribed');
+} else {
+    console.log('Whoops Error!')
 }
